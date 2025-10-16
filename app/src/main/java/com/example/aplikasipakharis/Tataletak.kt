2eea18ebe23dad7de19ba31ffa7c03cd93cbacaa
feraistanti <fistanti551@gmail.com>
2025-10-16 19:27:20 +0700

    isi Box dengan beberapa teks ditampilkan di tengah layout

--- app/src/main/java/com/example/aplikasipakharis/Tataletak.kt
+++ app/src/main/java/com/example/aplikasipakharis/Tataletak.kt
@@ -53,3 +53,12 @@ fun TataletakBox(modifier: Modifier) {
             .fillMaxHeight()
             .fillMaxWidth(),
         contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
